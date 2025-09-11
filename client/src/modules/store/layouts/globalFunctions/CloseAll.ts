543a9cfb61e498e8d7dbeed799709f94865c0cb7
abdullah ahmed <abdallaroom25@gmail.com>
2025-09-11 04:49:17 +0300

    whichlist & cart menwhichlist & cart menus with mask overlayus with mask overlay

--- client/src/modules/store/layouts/globalFunctions/CloseAll.ts
+++ client/src/modules/store/layouts/globalFunctions/CloseAll.ts
@@ -1,7 +1,8 @@
 import { changeCartMenuState } from "./Cart"
+import { changeWhichlistMenuState } from "./whichlist"
 
 
  export const CloseEveryMenu = ()=>{
     changeCartMenuState("close")
-    
+    changeWhichlistMenuState("close")
  }
