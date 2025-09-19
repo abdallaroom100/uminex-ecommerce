e7e456b0f78fdc2613809195530da30fff2bf4df
abdullah ahmed <abdallaroom25@gmail.com>
2025-09-20 02:24:46 +0300

    backend: add application models

---
+++ server/controllers/product.controller.js
@@ -0,0 +1,6 @@
+
+
+export const addProduct =  async ()=>{
+
+}
+
--- server/models/product.model.js
+++ server/models/product.model.js
@@ -1,23 +1,46 @@
 import mongoose from "mongoose";
 
+
+const RealNumbervalidation =  ()=>{
+    return v=>Array.isArray(v) && v.length > 0
+}
 const productSchema = new mongoose.Schema({
-    title:String,
-    starts:Number,
-    price:String,
-    mainDetails:[String],
-    quantity:Number,
-    contact:String,
-    shippingStatus:Boolean,
-    refundsDays:Number,
-    vendor:String,
-    categories:[String],
-    tags:[String],
-    descriptionCode:String,
-    mainImage:String,
-    images:[String]
-})
+  title: { type: String, required: true, trim: true },
+  starts: { type: Number, required: true, min: 0 },
+  price: { type: Number, required: true, min: 0 }, 
+  mainDetails: { 
+    type: [String], 
+    required: true, 
+    validate: RealNumbervalidation()
+  },
+  quantity: { type: Number, required: true, min: 0 },
+  contact: { 
+    type: String, 
+    required: true, 
+  },
+  shippingStatus: { type: Boolean, required: true },
+  refundsDays: { type: Number, required: true, min: 0 },
+  vendor: { type: String, required: true, trim: true },
+  keywords: [String], 
+  categories: { 
+    type: [String], 
+    required: true, 
+    validate: RealNumbervalidation()
+  },
+  tags: { 
+    type: [String], 
+    required: true, 
+    validate: RealNumbervalidation()
+  },
+  descriptionCode: { type: String, required: true },
+  mainImage: { type: String, required: true },
+  images: { 
+    type: [String], 
+    required: true,
+    validate: RealNumbervalidation()
+  }
+}, { timestamps: true });
 
+const Product = mongoose.model("Product", productSchema);
 
-const Product = mongoose.model("Product",productSchema)
-
-export default Product
+export default Product;
